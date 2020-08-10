5025eb629c13b97bec4b486f780124cad19f380c
Taimoor Tariq <taimoorhero15@gmail.com>
2020-08-10 15:26:39 -0400

    Initial Release

--- src/main/java/io/github/ann0y1nghacker/plugin/Plugin.java
+++ src/main/java/io/github/ann0y1nghacker/plugin/Plugin.java
@@ -101,7 +101,7 @@ public final class Plugin extends JavaPlugin {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     ChatColor wColor = ChatColor.GREEN;
                     String uuid = player.getUniqueId().toString();
-                    int afkMarker = 3;
+                    int afkMarker = 10;
 
                     try {
                         Object obj = new JsonParser().parse(new FileReader("./afkCordsData.json"));
